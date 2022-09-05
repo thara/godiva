22da82b8da8c2545b081696397f3d87d6ac62abc
Tomochika Hara <git@thara.dev>
2022-09-05 21:47:29 +0900

    :hammer: Embed cpInfo common field

--- class/cp_info.go
+++ class/cp_info.go
@@ -44,203 +44,191 @@ func parseCpInfo(r io.Reader) (cpInfo, error) {
 
 	var p parser
 
-	tag := b[0]
-	switch tag {
+	tag := cpInfoTag{tag: b[0]}
+	switch tag.tag {
 	case ConstantKindClass:
-		c := constantClass{tag: tag}
+		c := constantClass{cpInfoTag: tag}
 		p.readInteger(r, &c.nameIndex, "CONSTANT_Class_info's name_index")
 		return &c, p.Err
 	case ConstantKindFieldref:
-		c := constantFieldref{tag: tag}
+		c := constantFieldref{cpInfoTag: tag}
 		p.readInteger(r, &c.classIndex, "CONSTANT_Fieldref_info's class_index")
 		p.readInteger(r, &c.nameAndTypeIndex, "CONSTANT_Fieldref_info's name_and_type_index")
 		return &c, p.Err
 	case ConstantKindMethodref:
-		c := constantMethodref{tag: tag}
+		c := constantMethodref{cpInfoTag: tag}
 		p.readInteger(r, &c.classIndex, "CONSTANT_Methodref_info's class_index")
 		p.readInteger(r, &c.nameAndTypeIndex, "CONSTANT_Methodref_info's name_and_type_index")
 		return &c, p.Err
 	case ConstantKindInterfaceMethodref:
-		c := constantInterfaceMethodref{tag: tag}
+		c := constantInterfaceMethodref{cpInfoTag: tag}
 		p.readInteger(r, &c.classIndex, "CONSTANT_InterfaceMethodref_info's class_index")
 		p.readInteger(r, &c.nameAndTypeIndex, "CONSTANT_InterfaceMethodref_info's name_and_type_index")
 		return &c, p.Err
 	case ConstantKindString:
-		c := constantString{tag: tag}
+		c := constantString{cpInfoTag: tag}
 		p.readInteger(r, &c.stringIndex, "CONSTANT_String_info's string_index")
 		return &c, p.Err
 	case ConstantKindInteger:
-		c := constantInteger{tag: tag}
+		c := constantInteger{cpInfoTag: tag}
 		p.readBytes(r, c.bytes[:], "CONSTANT_Integer_info's bytes")
 		return &c, p.Err
 	case ConstantKindFloat:
-		c := constantFloat{tag: tag}
+		c := constantFloat{cpInfoTag: tag}
 		p.readBytes(r, c.bytes[:], "CONSTANT_Float_info's bytes")
 		return &c, p.Err
 	case ConstantKindLong:
-		c := constantLong{tag: tag}
+		c := constantLong{cpInfoTag: tag}
 		p.readBytes(r, c.high[:], "CONSTANT_Long_info's high_bytes")
 		p.readBytes(r, c.low[:], "CONSTANT_Long_info's low_bytes")
 		return &c, p.Err
 	case ConstantKindDouble:
-		c := constantDouble{tag: tag}
+		c := constantDouble{cpInfoTag: tag}
 		p.readBytes(r, c.high[:], "CONSTANT_Double_info's high_bytes")
 		p.readBytes(r, c.low[:], "CONSTANT_Double_info's low_bytes")
 		return &c, p.Err
 	case ConstantKindNameAndType:
-		c := constantNameAndType{tag: tag}
+		c := constantNameAndType{cpInfoTag: tag}
 		p.readInteger(r, &c.nameIndex, "CONSTANT_NameAndType_info's name_index")
 		p.readInteger(r, &c.descriptorIndex, "CONSTANT_NameAndType_info's descriptor_index")
 		return &c, p.Err
 	case ConstantKindUtf8:
-		c := constantUtf8{tag: tag}
+		c := constantUtf8{cpInfoTag: tag}
 		p.readInteger(r, &c.length, "CONSTANT_Utf8_info's length")
 		c.bytes = make([]byte, c.length)
 		p.readBytes(r, c.bytes[:], "CONSTANT_Utf8_info's bytes")
 		return &c, p.Err
 	case ConstantKindMethodHandle:
-		c := constantMethodHandle{tag: tag}
+		c := constantMethodHandle{cpInfoTag: tag}
 		p.readInteger(r, &c.referenceKind, "CONSTANT_MethodHandle_info's reference_kind")
 		p.readInteger(r, &c.referenceIndex, "CONSTANT_MethodHandle_info's reference_index")
 		return &c, p.Err
 	case ConstantKindMethodType:
-		c := constantMethodType{tag: tag}
+		c := constantMethodType{cpInfoTag: tag}
 		p.readInteger(r, &c.descriptorIndex, "CONSTANT_MethodType_info's descriptor_index")
 		return &c, p.Err
 	case ConstantKindDynamic:
-		c := constantDynamic{tag: tag}
+		c := constantDynamic{cpInfoTag: tag}
 		p.readInteger(r, &c.bootstrapMethodAttrIndex, "CONSTANT_Dynamic_info's bootstrap_method_attr_index")
 		p.readInteger(r, &c.nameAndTypeIndex, "CONSTANT_Dynamic_info's name_and_type_index")
 		return &c, p.Err
 	case ConstantKindInvokeDynamic:
-		c := constantInvokeDynamic{tag: tag}
+		c := constantInvokeDynamic{cpInfoTag: tag}
 		p.readInteger(r, &c.bootstrapMethodAttrIndex, "CONSTANT_Dynamic_info's bootstrap_method_attr_index")
 		p.readInteger(r, &c.nameAndTypeIndex, "CONSTANT_Dynamic_info's name_and_type_index")
 		return &c, p.Err
 	case ConstantKindModule:
-		c := constantModule{tag: tag}
+		c := constantModule{cpInfoTag: tag}
 		p.readInteger(r, &c.nameIndex, "CONSTANT_Module_info's name_index")
 		return &c, p.Err
 	case ConstantKindPackage:
-		c := constantPackage{tag: tag}
+		c := constantPackage{cpInfoTag: tag}
 		p.readInteger(r, &c.nameIndex, "CONSTANT_Module_info's name_index")
 		return &c, p.Err
 	}
-	return nil, fmt.Errorf("unsupported tag for cp_info: %d", tag)
+	return nil, fmt.Errorf("unsupported tag for cp_info: %d", tag.tag)
 }
 
+type cpInfoTag struct {
+	tag byte
+}
+
+func (c *cpInfoTag) Tag() byte { return c.tag }
+
 type constantClass struct {
-	tag       byte
+	cpInfoTag
 	nameIndex uint16
 }
 
 type constantFieldref struct {
-	tag              byte
+	cpInfoTag
 	classIndex       uint16
 	nameAndTypeIndex uint16
 }
 
 type constantMethodref struct {
-	tag              byte
+	cpInfoTag
 	classIndex       uint16
 	nameAndTypeIndex uint16
 }
 
 type constantInterfaceMethodref struct {
-	tag              byte
+	cpInfoTag
 	classIndex       uint16
 	nameAndTypeIndex uint16
 }
 
 type constantString struct {
-	tag         byte
+	cpInfoTag
 	stringIndex uint16
 }
 
 type constantInteger struct {
-	tag   byte
+	cpInfoTag
 	bytes [4]byte
 }
 
 type constantFloat struct {
-	tag   byte
+	cpInfoTag
 	bytes [4]byte
 }
 
 type constantLong struct {
-	tag  byte
+	cpInfoTag
 	high [4]byte
 	low  [4]byte
 }
 
 type constantDouble struct {
-	tag  byte
+	cpInfoTag
 	high [4]byte
 	low  [4]byte
 }
 
 type constantNameAndType struct {
-	tag             byte
+	cpInfoTag
 	nameIndex       uint16
 	descriptorIndex uint16
 }
 
 type constantUtf8 struct {
-	tag    byte
+	cpInfoTag
 	length uint16
 	bytes  []byte
 }
 
 type constantMethodHandle struct {
-	tag            byte
+	cpInfoTag
 	referenceKind  byte
 	referenceIndex uint16
 }
 
 type constantMethodType struct {
-	tag             byte
+	cpInfoTag
 	descriptorIndex uint16
 }
 
 type constantDynamic struct {
-	tag                      byte
+	cpInfoTag
 	bootstrapMethodAttrIndex uint16
 	nameAndTypeIndex         uint16
 }
 
 type constantInvokeDynamic struct {
-	tag                      byte
+	cpInfoTag
 	bootstrapMethodAttrIndex uint16
 	nameAndTypeIndex         uint16
 }
 
 type constantModule struct {
-	tag       byte
+	cpInfoTag
 	nameIndex uint16
 }
 
 type constantPackage struct {
-	tag       byte
+	cpInfoTag
 	nameIndex uint16
 }
-
-func (c *constantClass) Tag() byte              { return c.tag }
-func (c *constantFieldref) Tag() byte           { return c.tag }
-func (c *constantMethodref) Tag() byte          { return c.tag }
-func (c *constantInterfaceMethodref) Tag() byte { return c.tag }
-func (c *constantString) Tag() byte             { return c.tag }
-func (c *constantInteger) Tag() byte            { return c.tag }
-func (c *constantFloat) Tag() byte              { return c.tag }
-func (c *constantLong) Tag() byte               { return c.tag }
-func (c *constantDouble) Tag() byte             { return c.tag }
-func (c *constantNameAndType) Tag() byte        { return c.tag }
-func (c *constantUtf8) Tag() byte               { return c.tag }
-func (c *constantMethodHandle) Tag() byte       { return c.tag }
-func (c *constantMethodType) Tag() byte         { return c.tag }
-func (c *constantDynamic) Tag() byte            { return c.tag }
-func (c *constantInvokeDynamic) Tag() byte      { return c.tag }
-func (c *constantModule) Tag() byte             { return c.tag }
-func (c *constantPackage) Tag() byte            { return c.tag }
 
 func (c *constantClass) String() string { return fmt.Sprintf("#%d", c.nameIndex) }
 func (c *constantFieldref) String() string {
